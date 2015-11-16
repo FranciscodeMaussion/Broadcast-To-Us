6f67b19027ec6ba9b464825d482cd9352fdbef36
frandemo <franciscodemaussion@gmail.com>
2015-11-16 20:21:14 -0300

    log in logica

--- brodus/urls.py
+++ brodus/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
 urlpatterns = patterns('',
                        url(r'^$', 'brodus.views.index', name='index'),
+                       url(r'^user/log_in$','brodus.views.log_in', name='log_in'),
                       )
--- brodus/views.py
+++ brodus/views.py
@@ -3,8 +3,30 @@ from django.shortcuts import render_to_response, render, redirect
 from django.template import RequestContext
 from django.views.decorators.csrf import requires_csrf_token
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import requires_csrf_token
 # Create your views here.
 
 def index(request):
     context = RequestContext(request)
     return render_to_response('index.html', context)
+
+@requires_csrf_token
+def log_in(request):
+    context = RequestContext(request)
+    if request.method=='POST':
+        username=request.POST['username']
+        password=request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                return HttpResponse(status=202)
+            else:
+                return HttpResponse(status=203)
+        else:
+             return HttpResponse(status=203)
+    else:
+         return render_to_response('log_in_user.html', context)
