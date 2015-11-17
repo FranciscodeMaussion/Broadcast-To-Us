ea1b6ba9e2706a198f670b3a9081064b582a1286
frandemo <franciscodemaussion@gmail.com>
2015-11-16 22:16:49 -0300

    crear y loguearse

--- brodus/urls.py
+++ brodus/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('',
                        url(r'^$', 'brodus.views.index', name='index'),
                        url(r'^user/log_in$','brodus.views.log_in', name='log_in'),
+                       url(r'^user/new$','brodus.views.new_user', name='new_user'),
                       )
--- brodus/views.py
+++ brodus/views.py
@@ -30,3 +30,19 @@ def log_in(request):
              return HttpResponse(status=203)
     else:
          return render_to_response('log_in_user.html', context)
+
+@requires_csrf_token
+def new_user(request):
+    context = RequestContext(request)
+    if request.method=='POST':
+        n_u=User()
+        username=request.POST['username']
+        n_u.username=username
+        n_u.email=request.POST['email']
+        password=request.POST['password']
+        n_u.set_password(password)
+        n_u.save()
+        user = authenticate(username=username, password=password)
+        login(request, user)
+    return render_to_response('a_user.html',
+                              context)
